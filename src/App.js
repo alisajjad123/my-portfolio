749309174415e7356d741a44f7f86b71b7f802b9
Mac-23 <mac16@Ali.local>
2023-07-27 21:13:50 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const projects = [
+  {
+    title: 'Project 1',
+    description: 'Description of project 1.',
+    link: 'https://github.com/your-username/project1',
+  },
+  {
+    title: 'Project 2',
+    description: 'Description of project 2.',
+    link: 'https://github.com/your-username/project2',
+  },
+  // Add more projects as needed
+];
+
+const Portfolio = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header>
+        <h1>Ali Sajjad</h1>
+        <p>Welcome to my portfolio showcasing some of my projects.</p>
       </header>
+      <div className="projects">
+        {projects.map((project, index) => (
+          <div className="project" key={index}>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              View on GitHub
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Portfolio;
